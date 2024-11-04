917c4c3ce8274e4f8faecdb27ac658f35fcf2c23
Aniket Sompura <aniketpruthvi@gmail.com>
2024-11-04 11:36:43 +0530

    Create ListPage.js

---
+++ web-tech/src/ListPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListPage = () => {
+  return (
+    <div>
+      <h2>Your Media List</h2>
+      <p>This is where you can view and manage your media list.</p>
+    </div>
+  );
+};
+
+export default ListPage;
