e0b430de7c453e339c1641c967599689f98ae12a
Aniket Sompura <aniketpruthvi@gmail.com>
2024-11-12 15:32:20 +0530

    functionality for register and login

--- web-tech/src/RegisterPage.js
+++ web-tech/src/RegisterPage.js
@@ -1,28 +1,26 @@
-// RegisterPage.js
 import React from 'react';
-import './RegisterPage.css'; // Create a CSS file for custom styling
+import './RegisterPage.css';
 
-const RegisterPage = () => {
-  const handleRegisterSubmit = (event) => {
-    event.preventDefault();
-    console.log('Register form submitted');
-    // Add logic to handle form submission, such as API calls or validation
-  };
+const RegisterPage = ( {onRegisterClick, onLoginClick} ) => {
 
   return (
     <div className="register-page">
       <h2>Register an Account</h2>
-      <form onSubmit={handleRegisterSubmit}>
+      <form>
         <input type="text" placeholder="Full Name" required />
         <input type="email" placeholder="Email" required />
         <input type="text" placeholder="Username" required />
         <input type="password" placeholder="Password" required />
         <input type="password" placeholder="Confirm Password" required />
 
-        <button type="submit">Register</button>
+        <button type="submit" onClick={() => {
+          onRegisterClick();
+        }}>
+          Register
+          </button>
       </form>
       <p>
-        Already have an account? <a href="/login" style={{ color: 'blue' }}>Login</a>
+        Already have an account? <a href="/login" style={{ color: 'blue' }} onClick={onLoginClick}>Login</a>
       </p>
     </div>
   );
